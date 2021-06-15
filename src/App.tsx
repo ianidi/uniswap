431bc013e5692dc0b00356e1a7d231bc2eac4dd3
ianidi <51965970+ianidi@users.noreply.github.com>
2021-06-15 18:47:41 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { gql, useQuery, NetworkStatus } from "@apollo/client";
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { TableComponent } from './Table';
+import { Datepicker } from './components/datepicker';
+//, timestamp_gte: "10000000000", timestamp_lte: "10000000000"
+const SWAPS_QUERY = gql`
+  query swaps($sender: String) {
+    swaps(orderBy: timestamp, orderDirection: desc, where: { sender: $sender }) {
+      id
+      transaction { id }
+      sender
+      timestamp
+      sender
+      recipient
+      origin
+      token0 { symbol, name }
+      token1 { symbol, name }
+      amount0
+      amount1
+      amountUSD
+    }
+  }
+`;
+
+const AppContext = React.createContext('');
+
+const Swaps = () => {
+  const sender = useContext(AppContext);
+
+  const { loading, error, data, refetch, networkStatus } = useQuery(SWAPS_QUERY, {
+    variables: { sender },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [sender])
+
+  if (loading || networkStatus === NetworkStatus.refetch) {
+    return <CircularProgress size={20} />;
+  }
+
+  if (error) {
+    return <>{error.message}</>;
+  }
+
+  return (
+    <>
+      {data && data.swaps && data.swaps.length > 0 && <TableComponent rows={data.swaps} headCells={headCells} />}
+    </>
+  )
+};
+
+const headCells = [
+  { id: "tx", disablePadding: true, label: "tx" },
+  { id: "description", disablePadding: false, label: "description" },
+  { id: "date", disablePadding: false, label: "date" },
+  { id: "sender", disablePadding: false, label: "sender" },
+  { id: "recipient", disablePadding: false, label: "recipient" },
+  { id: "amountUSD", disablePadding: false, label: "USD amount" },
+];
+
+const App = () => {
+
+
+  return (
+    <div>
+      <Form>
+        <Swaps />
+      </Form>
+    </div>
+  )
+};
+
+
+function Form({ children }) {
+  const [wallet, setWallet] = useState("0xe592427a0aece92de3edee1f18e0157c05861564");
+  const [sender, setSender] = useState("");
+  const [dateFrom, setDateFrom] = React.useState(new Date('2021-01-01T00:00:00'));
+  const [dateTo, setDateTo] = React.useState(new Date('2021-06-01T00:00:00'));
+
+  const update = () => {
+    setSender(wallet)
+  }
+
+  const handleDateFromChange = (date) => {
+    setDateFrom(date);
+  };
+
+  const handleDateToChange = (date) => {
+    setDateTo(date);
+  };
+
+  return (
+    <div>
+      <AppContext.Provider value={sender}>
+        <form noValidate autoComplete="off">
+          <TextField label="Wallet address" value={wallet} onChange={e => setWallet(e.target.value)} style={{ minWidth: 400 }} />
+        </form>
+        <br />
+
+        <Grid container alignItems="flex-start">
+          <Datepicker callback={handleDateFromChange} value={dateFrom} placeholder="Date from" />
+          <Datepicker callback={handleDateToChange} value={dateTo} placeholder="Date to" />
+        </Grid>
+
+        <br />
+
+        <Button variant="contained" color="primary" onClick={update}>Search</Button>
+        <br />
+        {children}
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/datepicker.tsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import DatePicker from "react-modern-calendar-datepicker";
+import './datepicker.css'
+
+export const Datepicker = ({ callback, value, placeholder }) => {
+    //@ts-ignore
+    let defaultValue = {
+        year: value.getFullYear(),
+        month: value.getMonth() + 1,
+        day: value.getDate(),
+    };
+
+    const myCustomLocale = {
+        // months list by order
+        months: [
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+        ],
+
+        // week days by order
+        weekDays: [
+            {
+                name: "Понедельник",
+                short: "ПН",
+            },
+            {
+                name: "Вторник",
+                short: "ВТ",
+            },
+            {
+                name: "Среда",
+                short: "СР",
+            },
+            {
+                name: "Четверг",
+                short: "ЧТ",
+            },
+            {
+                name: "Пятница",
+                short: "ПТ",
+            },
+            {
+                name: "Суббота",
+                short: "СБ",
+                isWeekend: true,
+            },
+            {
+                name: "Воскресенье", // used for accessibility
+                short: "ВС", // displayed at the top of days' rows
+                isWeekend: true, // is it a formal weekend or not?
+            },
+        ],
+
+        // just play around with this number between 0 and 6
+        weekStartingIndex: 6,
+
+        // return a { year: number, month: number, day: number } object
+        getToday(gregorainTodayObject) {
+            return gregorainTodayObject;
+        },
+
+        // return a native JavaScript date here
+        toNativeDate(date) {
+            return new Date(date.year, date.month - 1, date.day);
+        },
+
+        // return a number for date's month length
+        getMonthLength(date) {
+            return new Date(date.year, date.month, 0).getDate();
+        },
+
+        // return a transformed digit to your locale
+        transformDigit(digit) {
+            return digit;
+        },
+
+        // texts in the date picker
+        nextMonth: "Следующий",
+        previousMonth: "Предыдущий",
+        openMonthSelector: "Открыть",
+        openYearSelector: "Открыть",
+        closeMonthSelector: "Закрыть",
+        closeYearSelector: "Закрыть",
+        defaultPlaceholder: "",
+
+        // for input range value
+        from: "От",
+        to: "До",
+
+        // used for input value when multi dates are selected
+        digitSeparator: ",",
+
+        // if your provide -2 for example, year will be 2 digited
+        yearLetterSkip: 0,
+
+        // is your language rtl or ltr?
+        isRtl: false,
+    };
+
+    const [selectedDay, setSelectedDay] = useState(defaultValue);
+
+    const renderCustomInput = ({ ref }) => (
+        <input
+            readOnly
+            ref={ref}
+            placeholder={placeholder}
+            //@ts-ignore
+            value={selectedDay ? `${selectedDay.day}/${selectedDay.month}/${selectedDay.year}` : ""}
+            className="input datepicker__input"
+        />
+    );
+
+    return (
+        <React.Fragment>
+            <div className="datepicker__wrapper">
+                {placeholder}
+                <DatePicker
+                    value={selectedDay}
+                    onChange={(val) => {
+                        callback(val);
+                        //@ts-ignore
+                        setSelectedDay(val);
+                    }}
+                    renderInput={renderCustomInput}
+                    locale={myCustomLocale}
+                    shouldHighlightWeekends
+                    // maximumDate={utils().getToday()}
+                    calendarPopperPosition="bottom"
+                    colorPrimary="#2fa8ec"
+                    colorPrimaryLight="#2fa8ec"
+                    calendarClassName="datepicker"
+                />
+            </div>
+        </React.Fragment>
+    );
+};
