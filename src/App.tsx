6ac0e872602ab4ce5049584745c1e9a6c2952193
ianidi <51965970+ianidi@users.noreply.github.com>
2021-06-15 18:53:20 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { gql, useQuery, NetworkStatus } from "@apollo/client";
+import { getUnixTime } from "date-fns"
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -26,10 +27,10 @@ const SWAPS_QUERY = gql`
   }
 `;
 
-const AppContext = React.createContext('');
+const AppContext = React.createContext({ sender: '' });
 
 const Swaps = () => {
-  const sender = useContext(AppContext);
+  const { sender } = useContext(AppContext);
 
   const { loading, error, data, refetch, networkStatus } = useQuery(SWAPS_QUERY, {
     variables: { sender },
@@ -56,11 +57,11 @@ const Swaps = () => {
 };
 
 const headCells = [
-  { id: "tx", disablePadding: true, label: "tx" },
-  { id: "description", disablePadding: false, label: "description" },
-  { id: "date", disablePadding: false, label: "date" },
-  { id: "sender", disablePadding: false, label: "sender" },
-  { id: "recipient", disablePadding: false, label: "recipient" },
+  { id: "tx", disablePadding: true, label: "TX" },
+  { id: "description", disablePadding: false, label: "Description" },
+  { id: "date", disablePadding: false, label: "Date" },
+  { id: "sender", disablePadding: false, label: "Sender" },
+  { id: "recipient", disablePadding: false, label: "Recipient" },
   { id: "amountUSD", disablePadding: false, label: "USD amount" },
 ];
 
@@ -97,7 +98,7 @@ function Form({ children }) {
 
   return (
     <div>
-      <AppContext.Provider value={sender}>
+      <AppContext.Provider value={{ sender }}>
         <form noValidate autoComplete="off">
           <TextField label="Wallet address" value={wallet} onChange={e => setWallet(e.target.value)} style={{ minWidth: 400 }} />
         </form>
